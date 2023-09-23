0f7e2fc3dbb4df7bb768f1cfd0f015dfcaccdcaa
JayanthiSaravanan <jayanthi.saravanann@gmail.com>
2023-09-24 00:49:04 +0530

    Single level Inheritance

---
+++ testleaf/TestData.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class TestData {
+
+	public void enterCredentials() {
+		System.out.println("Enter Credentials:User name and Password");
+
+	}
+
+	public void navigateToHomepage() {
+		System.out.println("Navigate to HomePage:Amazon");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
