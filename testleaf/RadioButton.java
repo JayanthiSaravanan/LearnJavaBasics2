6a8f2ed5d44307812d390d484d8326f66e7263c8
JayanthiSaravanan <jayanthi.saravanann@gmail.com>
2023-09-24 07:35:20 +0530

    Hybrid Inheritance

---
+++ testleaf/RadioButton.java
@@ -0,0 +1,16 @@
+package week3;
+
+public class RadioButton extends Button {
+
+	
+	public void selectRadioButton() {
+		System.out.println("Click Radio Button");
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
+
