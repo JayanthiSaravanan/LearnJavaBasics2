b1346e9e2f699c52a97122b03328846aec74c807
JayanthiSaravanan <jayanthi.saravanann@gmail.com>
2023-09-24 07:36:29 +0530

    Hybrid Inheritance

---
+++ testleaf/CheckBoxButton.java
@@ -0,0 +1,16 @@
+package week3;
+
+public class CheckBoxButton extends RadioButton {
+
+	
+	public void clickCheckButton() {
+		System.out.println( "Click ChecBox");
+
+	}
+
+public static void main(String[] args)
+{
+	
+}
+}
+
