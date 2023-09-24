d523665f6fd22b73b3c745b00dc3c1695c188988
JayanthiSaravanan <jayanthi.saravanann@gmail.com>
2023-09-24 07:30:45 +0530

    Hierarchical Inheritance

---
+++ testleaf/WebElement.java
@@ -0,0 +1,19 @@
+package week3;
+
+public class WebElement {
+
+	
+	public void click() {
+		System.out.println(" ");
+		
+	}
+	public void setText(String Text) {
+		System.out.println("");
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
